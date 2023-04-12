79492bf01181aaa6b448869dbf9faf239bf4a7e1
GabrielaVidal7 <ga28bvidal@gmail.com>
2023-04-12 15:43:27 -0300

    Dataset atualizado

--- move_files.py
+++ move_files.py
@@ -1,20 +1,22 @@
 import os
 import shutil
 
-txt_train = [f for f in os.listdir("Dataset/test/") if '.txt' in f.lower()]
-images_train = [f for f in os.listdir("Dataset/test/") if '.jpg' in f.lower()]
+txt_test = [f for f in os.listdir("Dataset/test/") if '.txt' in f.lower()]
+images_test = [f for f in os.listdir("Dataset/test/") if '.jpg' in f.lower()]
 
 txt_valid = [f for f in os.listdir("Dataset/validation/") if '.txt' in f.lower()]
 images_valid = [f for f in os.listdir("Dataset/validation/") if '.jpg' in f.lower()]
 
-os.mkdir('Dataset/test/labels')
+txt_train = [f for f in os.listdir("Dataset/train/") if '.txt' in f.lower()]
+images_train = [f for f in os.listdir("Dataset/train/") if '.jpg' in f.lower()]
 
-for text in txt_train:
+os.mkdir('Dataset/test/labels')
+for text in txt_test:
     new_path = 'Dataset/test/labels/' + text
     shutil.move('Dataset/test/'+text, new_path)
 
 os.mkdir('Dataset/test/images')
-for image in images_train:
+for image in images_test:
     new_path = 'Dataset/test/images/' + image
     shutil.move('Dataset/test/'+image, new_path)
 
@@ -28,3 +30,12 @@ for image in images_valid:
     new_path = 'Dataset/validation/images/' + image
     shutil.move('Dataset/validation/'+image, new_path)
 
+os.mkdir('Dataset/train/labels')
+for text in txt_train:
+    new_path = 'Dataset/train/labels/' + text
+    shutil.move('Dataset/train/'+text, new_path)
+
+os.mkdir('Dataset/train/images')
+for image in images_train:
+    new_path = 'Dataset/train/images/' + image
+    shutil.move('Dataset/train/'+image, new_path)
